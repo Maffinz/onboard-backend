d4e073fe176bff6f1637d07d9824a195c776b944
William <williyaaj@gmail.com>
2019-07-31 11:11:38 -0400

    Connection to DB

--- server/routes/api.py
+++ server/routes/api.py
@@ -1,6 +1,5 @@
 from server import app
 from flask import render_template, Flask
-# from flask_db2 import DB2
 import ibm_db
 
 # app.config['DB2_DATABASE'] = 'BLUDB'
@@ -14,7 +13,16 @@ import ibm_db
 
 # cur = db.connection.cursor()
 
+conn = ibm_db.connect('DATABASE=BLUDB;'
+                     'HOSTNAME=dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net;'  # 127.0.0.1 or localhost works if it's local
+                     'PORT=50000;'
+                     'PROTOCOL=TCPIP;'
+                     'UID=gqf91534;'
+                     'PWD=n8rbtmpr-0nfphqs;', '', '')
+
 @app.route("/api/get_all")
 def get_all():
-    return "api/get_all"
+    if conn:
+        return "Conncected to DB"
+    return "not connected"
     # return cur.execute("SELECT * FROM user")
