ab1bbc6ba558921e308b545054fbdffee2a0a261
William <williyaaj@gmail.com>
2019-07-31 10:04:37 -0400

    database name change

--- server/routes/api.py
+++ server/routes/api.py
@@ -1,9 +1,9 @@
 from server import app
 from flask import render_template, Flask
-from flask_db2 import DB2
-# import ibm_db
+# from flask_db2 import DB2
+import ibm_db
 
-# app.config['DB2_DATABASE'] = 'gqf91534'
+# app.config['DB2_DATABASE'] = 'BLUDB'
 # app.config['DB2_HOSTNAME'] = 'dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net'
 # app.config['DB2_PORT'] = 50000
 # app.config['DB2_PROTOCOL'] = 'TCPIP'
